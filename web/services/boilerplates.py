83110f784fd8843cda8dcc12d1788e7d9e47bb54
kush ahuja <ahujakush07@gmail.com>
2026-09-10 01:18:29 +0530

    Update C++ boilerplate to use std namespace

--- web/services/boilerplates.py
+++ web/services/boilerplates.py
@@ -26,8 +26,10 @@ LANGUAGE_BOILERPLATES: Dict[str, Dict[str, str]] = {
         "code": (
             "#include <iostream>\n"
             "\n"
+            "using namespace std;\n"
+            "\n"
             "int main() {\n"
-            "    std::cout << \"Hello, World!\" << std::endl;\n"
+            "    cout << \"Hello, World!\" << endl;\n"
             "    return 0;\n"
             "}\n"
         ),
